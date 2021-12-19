6c4690b732f1c4ba62bc35819d5686e56a365fba
YuChenLiao <yuchenliao3@gmail.com>
2021-12-19 16:51:03 +0800

    开始构建右侧固钉

---
+++ src/components/rightAffix.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Affix, Dropdown, Button } from "antd"
+import 'antd/dist/antd.css'
+
+const RightAffix = () => {
+  return (
+    <Affix>
+      <Dropdown></Dropdown>
+    </Affix>
+  )
+}
