87a19226dfd802d32630a5e85476153391893b6d
YuChenLiao <yuchenliao3@gmail.com>
2021-12-19 15:30:09 +0800

    实现瀑布流样式

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -3,75 +3,105 @@ import axios from "axios"
 import './mainPage.css'
 
 const MainPage = () => {
-  const [imgsrc,setimgsrc] = useState([]);
-  const [domWidth, setWidth] = useState(0);
-  const waterfallImgRight = 10;
-  const [waterfallImgCol, setCol] = useState(0);
-  const waterfallImgBottom = 10;
-  const [waterfallDeviationHeight, setDeviation] = useState(
-    new Array(waterfallImgCol)
-  );
+  const [imgsrc,setimgsrc] = useState([]);  // 保存图片信息
+  let domWidth = 0; // 获取容器宽度
+  let waterfallImgRight = 0;  // 计算每张图片的x轴位置
+  const waterfallImgBottom = 10; // 等效每张图片的margin-bottom，目的是设置图片的下边距
+  let waterfallDeviationHeight = new Array(5);// 记录每一列的高度
 
 
   useEffect(() => {
-    setWidth(document.body.clientWidth)
     loadImg()
   }, [])
 
+  // 加载图片并计算图片位置
   const loadImg = async() => {
-    for ( let k = 0;k < waterfallDeviationHeight.length; k += 1) {
-      setDeviation(waterfallDeviationHeight[k] = 0)
+    const dtemp = document.getElementsByClassName('wContent');
+    domWidth = (dtemp[0].clientWidth);  // 获取容器宽度
+    const itemWidth = Math.floor(domWidth*0.18);  // 计算图片宽度
+    waterfallImgRight = Math.floor(domWidth*0.02);  // 计算图片位置
+    for ( let k = 0;k < waterfallDeviationHeight.length; k += 1) {  // 初始化每列高度
+      waterfallDeviationHeight[k] = 0;
     }
     try {
       axios.defaults.headers.common['x-api-key'] = '40a43cad-bb75-475b-bf26-c6a60506a908'; // Replace this with your API Key
       const res = await axios.get(
         'https://api.thecatapi.com/v1/images/search', 
-        { params: { limit: 40, size: 'full' } }
+        { params: { limit: 20, size: 'full' } }
       );
       const temp = res.data;
-      console.log(temp)
+      let url = [
+        {
+          id: '',
+          url: '',
+          width: 0,
+          height: 0,
+          top: 0,
+          left: 0,
+        }
+      ]
       for(let i = 0;i < temp.length;i++) {
         const pic = {
-          id: temp[0].id,
-          url: temp[0].url,
-          width: temp[0].width,
-          height: temp[0].height,
+          id: temp[i].id,
+          url: temp[i].url,
+          width: temp[i].width,
+          height: temp[i].height,
           top: 0,
           left: 0,
         };
         // 计算缩放后的高度
-        pic.height = Math.ceil((160/pic.width)*temp[0].height) + 30;
+        pic.height = Math.ceil((itemWidth/pic.width)*temp[i].height) + 30;
         // 获取高度最低列
-        let minIndex = waterfallDeviationHeight.indexOf(Math.min.apply(null, waterfallDeviationHeight));
+        let minIndex = waterfallDeviationHeight.indexOf(Math.min(...waterfallDeviationHeight));
         // 获取图片顶部位置
         pic.top = waterfallDeviationHeight[minIndex];
-        pic.left = minIndex*(waterfallImgRight + 170);
+        pic.left = minIndex*(waterfallImgRight + itemWidth);
         waterfallDeviationHeight[minIndex] += pic.height + waterfallImgBottom;
-        console.log('珍爱生命，远离eslint');
-        setimgsrc([...imgsrc,pic])
+        url[i] = pic;
       }
-      console.log(imgsrc)
-      // console.log('url:', this.imgsrc[i].url);
-      // console.log(this.imgsrc[i].left);
-      // console.log(this.domWidth);
-      // console.log(this.waterfallDeviationHeight.length)
-      // console.log(minIndex);
-      // console.log(this.waterfallImgCol);
+      setimgsrc(url)
     } catch (err) {
       console.log(err);
+      alert('加载出错');
     }
   };
 
+  // 瀑布流组件
+  const Waterfall = (...src) => {
+    const temp = src[0].src
+    if(!temp[0]) {
+      return null;
+    }
+    else
+      return (
+        temp.map((item,index)=>(
+          <div 
+            key={index}
+            className='wImg'
+            style={{
+              height: item.height,
+              top: item.top+'px',
+              left: item.left+'px'
+            }}
+          >
+            <img src={item.url} alt={index} className='imgItem'></img>
+          </div>
+        )
+      ))
+  }
+
   return (
     <div className="mainContent">
-      {
-        imgsrc.map((item,index)=>{
-          console.log(item)
-          return (
-            <div>内容{item.url}</div>
-          )
-        })
-      }
+      <div className="wContent">
+        <Waterfall src={imgsrc}></Waterfall>
+      </div>
+      <div style={{
+        height:'1px',
+        marginTop:'-1px',
+        clear: 'both',
+        overflow:'hidden',
+      }}
+      ></div> 
     </div>
   );
 }
