ed0d365bcdd38c69f2540a4809b87c79ab810e8a
YuChenLiao <yuchenliao3@gmail.com>
2021-12-21 01:03:04 +0800

    完成加载更多功能

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -6,10 +6,12 @@ import './mainPage.css'
 
 const MainPage = () => {
   const [imgsrc,setimgsrc] = useState([]);  // 保存图片信息
+  const [loaded,setLoaded] = useState(false); // 控制页脚加载
+  const [H,setH] = useState(0); // 控制页脚位置
   let domWidth = 0; // 获取容器宽度
   let waterfallImgRight = 0;  // 计算每张图片的x轴位置
   const waterfallImgBottom = 10; // 等效每张图片的margin-bottom，目的是设置图片的下边距
-  let waterfallDeviationHeight = new Array(5);// 记录每一列的高度
+  const [waterfallDeviationHeight,setWfH] = useState( new Array(0,0,0,0,0));// 记录每一列的高度
 
 
   useEffect(() => {
@@ -22,6 +24,7 @@ const MainPage = () => {
     domWidth = (dtemp[0].clientWidth);  // 获取容器宽度
     const itemWidth = Math.floor(domWidth*0.18);  // 计算图片宽度
     waterfallImgRight = Math.floor(domWidth*0.02);  // 计算图片位置
+    let waterfallDeviationHeight = new Array(5)
     for ( let k = 0;k < waterfallDeviationHeight.length; k += 1) {  // 初始化每列高度
       waterfallDeviationHeight[k] = 0;
     }
@@ -65,7 +68,10 @@ const MainPage = () => {
         waterfallDeviationHeight[minIndex] += pic.height + waterfallImgBottom;
         url[i] = pic;
       }
-      setimgsrc(url)
+      setH(Math.max(...waterfallDeviationHeight));
+      setimgsrc(url);
+      setLoaded(true);
+      setWfH(waterfallDeviationHeight)
     } catch (err) {
       console.log(err);
       alert('加载出错');
@@ -122,16 +128,97 @@ const MainPage = () => {
       ))
   }
 
+  const loadMore = async(any) => {
+    console.log(any)
+    let [...waterfallDeviationHeight] = any
+    const dtemp = document.getElementsByClassName('wContent');
+    domWidth = (dtemp[0].clientWidth);  // 获取容器宽度
+    const itemWidth = Math.floor(domWidth*0.18);  // 计算图片宽度
+    waterfallImgRight = Math.floor(domWidth*0.02);  // 计算图片位置
+    try {
+      axios.defaults.headers.common['x-api-key'] = '40a43cad-bb75-475b-bf26-c6a60506a908'; // Replace this with your API Key
+      const res = await axios.get(
+        'https://api.thecatapi.com/v1/images/search', 
+        { params: { limit: 20, size: 'full' } }
+      );
+      const temp = res.data;
+      let url = [
+        {
+          id: '',
+          url: '',
+          width: 0,
+          height: 0,
+          realWidth: 0,
+          realHeight: 0,
+          top: 0,
+          left: 0,
+        }
+      ]
+      for(let i = 0;i < temp.length;i++) {
+        const pic = {
+          id: temp[i].id,
+          url: temp[i].url,
+          width: temp[i].width,
+          height: temp[i].height,
+          realWidth: temp[i].width,
+          realHeight: temp[i].height,
+          top: 0,
+          left: 0,
+        };
+        // 计算缩放后的高度
+        pic.height = Math.ceil((itemWidth/pic.width)*temp[i].height) + 30;
+        // 获取高度最低列
+        let minIndex = waterfallDeviationHeight.indexOf(Math.min(...waterfallDeviationHeight));
+        // 获取图片顶部位置
+        pic.top = waterfallDeviationHeight[minIndex];
+        pic.left = minIndex*(waterfallImgRight + itemWidth);
+        waterfallDeviationHeight[minIndex] += pic.height + waterfallImgBottom;
+        url[i] = pic;
+      }
+      setH(Math.max(...waterfallDeviationHeight));
+      setimgsrc(imgsrc.concat(url));
+      setLoaded(true);
+      setWfH(waterfallDeviationHeight)
+    } catch (err) {
+      console.log(err);
+      alert('加载出错');
+    }
+  }
+
+  const Footer = (any) => {
+    let {loaded} = any
+    let {top} = any
+    if(!loaded)
+      return null;
+    else
+      return(
+        <div
+          style={{
+            position: 'absolute',
+            top: top + 80 + 'px',
+            width: '100%',
+            height: '.5rem',
+            left: '0',
+            lineHeight: '.5rem'
+          }}
+          className="footer"
+          onClick={()=>loadMore(waterfallDeviationHeight)}
+        >点击加载更多</div>
+      )
+  }
+
   return (
     <div className="mainContent">
       <div className="wContent">
         <Waterfall src={imgsrc}></Waterfall>
       </div>
+      <Footer loaded={loaded} top={H}></Footer>
       <div style={{
         height:'1px',
         marginTop:'-1px',
         clear: 'both',
         overflow:'hidden',
+        boxSizing: 'border-box'
       }}
       ></div> 
     </div>
