1e0644e71dca0ad562266c7754b280ed3e1b3e02
YuChenLiao <yuchenliao3@gmail.com>
2021-12-20 14:38:26 +0800

    添加鼠标悬停效果

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderTab from './components/header';
 import MainPage from './components/mainPage';
+import RightAffix from './components/rightAffix';
 
 function App() {
   return (
     <div className="App">
       <HeaderTab></HeaderTab>
       <MainPage></MainPage>
+      <RightAffix></RightAffix>
     </div>
   );
 }
--- src/components/rightAffix.js
+++ src/components/rightAffix.js
@@ -1,11 +1,31 @@
-import React from "react";
-import { Affix, Dropdown, Button } from "antd"
+import React from "react"
+import { Affix, Dropdown, Button, Menu } from "antd"
 import 'antd/dist/antd.css'
+import './rightAffix.css'
 
 const RightAffix = () => {
+
+  const menu = (
+      <Menu>
+        
+      </Menu>
+  )
+
   return (
-    <Affix>
-      <Dropdown></Dropdown>
+    <Affix 
+      style={{
+        position: 'absolute',
+        top: '65%',
+        left: '94%'
+      }}>
+      <Dropdown 
+        overlay={menu}
+        placement="topCenter"
+      >
+        <div className="mStart">123</div>
+      </Dropdown>
     </Affix>
   )
 }
+
+export default RightAffix
